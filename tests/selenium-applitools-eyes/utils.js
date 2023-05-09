cbc17b842c01270b6deb7373f56c2d7fea1e8df2
Gabriel Coelho da Cunha <43345507+gabrielcoelhodacunha@users.noreply.github.com>
2023-05-09 08:39:53 -0300

    Added some comments to document configurations

--- tests/selenium-applitools-eyes/utils.js
+++ tests/selenium-applitools-eyes/utils.js
@@ -12,38 +12,57 @@ const APPLITOOLS_APP = 'TMDB Web App';
 
 let eyesConfig;
 let eyes;
+// Permite utilizacao de mesma instancia em outros arquivos
 const getEyesConfig = () => eyesConfig;
 const getEyes = () => eyes;
 
+/**
+ * @summary Configura passos anteriores e posteriores dos testes com Applitools Eyes
+ */
 function configureApplitoolsEyesSetupAndTeardown() {
 	let runner;
-
+	// Antes de todos os testes
 	beforeAll(async () => {
+		// Cria instancia de configuracao
 		eyesConfig = new Configuration();
+		// Carrega a chave da API das variaveis de ambiente
 		eyesConfig.setApiKey(process.env.APPLITOOLS_API_KEY);
+		// Cria instancia para gerenciar os testes localmente
 		runner = new ClassicRunner();
 	});
-
+	// Antes de cada teste
 	beforeEach(async () => {
+		// Cria instancia do Eyes, que realiza as comparacoes
 		eyes = new Eyes(runner);
+		// Define que a instancia Eyes utilize a configuracao atual
 		eyes.setConfiguration(eyesConfig);
 		await eyes.open(
+			// Objeto driver a ser observado
 			getDriver(),
+			// Nome da aplicacao a ser testada
 			APPLITOOLS_APP,
+			// Nome do teste atual
 			expect.getState().currentTestName,
+			// Resolucao a ser testada de 1024x768
 			new RectangleSize(SCREEN.WIDTH, SCREEN.HEIGHT)
 		);
 	});
-
+	// Apos cada teste
 	afterEach(async () => {
+		// Fecha instancia do Eyes para que os testes fiquem disponiveis no painel da Applitools
 		await eyes.closeAsync();
 	});
-
+	// Apos todos os testes
 	afterAll(async () => {
-		// await runner.getAllTestResults();
+		// Recupera todos os resultados dos testes
+		await runner.getAllTestResults();
 	});
 }
 
+/**
+ * @summary Define a qual conjunto de testes o teste atual pertence de acordo com a pagina 'page'
+ * @param {string} page
+ */
 async function setBatchInfoForPage(page) {
 	await getEyesConfig().setBatch(new BatchInfo(`${APPLITOOLS_APP} @ ${page}`));
 }
--- tests/selenium/utils.js
+++ tests/selenium/utils.js
@@ -11,36 +11,60 @@ const SCREEN = {
 };
 
 let driver;
+// Permite utilizacao de mesma instancia em outros arquivos
 const getDriver = () => driver;
 
+/**
+ * @summary Configura passos anteriores e posteriores dos testes Selenium
+ */
 function configureSeleniumSetupAndTeardown() {
+	// Antes de cada teste
 	beforeEach(async () => {
+		// Cria e configura instancia de Chrome Web Driver.
 		driver = await configureChromeWebDriver();
+		// Acessa a pagina da aplicação
 		await getPage();
 	});
-
+	// Apos cada teste
 	afterEach(async () => {
+		// Fecha a instancia do Chrome Web Driver
 		await driver.quit();
 	});
 }
 
+/**
+ * @summary Cria e configura instancia de Chrome Web Driver.
+ */
 async function configureChromeWebDriver() {
+	// Cria instancia
 	const driver = new Builder()
 		.withCapabilities({
+			// define navegador
 			browserName: 'chrome',
+			// habilita configuracoes adicionais
 			'goog:chromeOptions': {
 				args: [],
 			},
 		})
 		.build();
+	// Configura o tempo de espera para cada passo da instancia em 1 segundo
 	await driver.manage().setTimeouts({ implicit: 1000 });
-	await driver
-		.manage()
-		.window()
-		.setRect({ x: 0, y: 0, width: SCREEN.WIDTH, height: SCREEN.HEIGHT });
+	// Configura a janela da instancia
+	await driver.manage().window().setRect({
+		// topo da pagina
+		x: 0,
+		y: 0,
+		// resolucao de 1024x768
+		width: SCREEN.WIDTH,
+		height: SCREEN.HEIGHT,
+	});
 	return driver;
 }
 
+/**
+ * @summary Acessa localhost na porta 3000 na page
+ * @param {string} page
+ */
 async function getPage(page = '') {
 	await getDriver().get(`http://localhost:3000/${page}`);
 }
