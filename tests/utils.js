100f41da45b635e5a0596851a104937739c27dda
Gabriel Coelho da Cunha <43345507+gabrielcoelhodacunha@users.noreply.github.com>
2023-05-09 08:59:35 -0300

    Added JSDocs to generateRandomString function

--- tests/utils.js
+++ tests/utils.js
@@ -1,3 +1,7 @@
+/**
+ * @summary Gera string de caracteres aleatorios de acordo com o tamanho 'length'
+ * @param {number} length
+ */
 const generateRandomString = (length = 20) =>
 	Array.from(Array(length), () =>
 		Math.floor(Math.random() * 36).toString(36)
